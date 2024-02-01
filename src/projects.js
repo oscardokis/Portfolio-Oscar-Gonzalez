b8b618028a34a3d2b2be65fd81d93441c4431f5a
Oscar Gonzalez <oscar-gonzalezd1@hotmail.com>
2024-01-31 22:13:04 -0600

    deleting the / in img src to fix a bug

--- src/projects.js
+++ src/projects.js
@@ -1,8 +1,8 @@
 export const projects = [
-    {title:"Recipes and Drinks", buttonTexts: ["React", "Tailwind", "JavaScript", "Vite"], img:"/RecipesAndDrinks.jpg", url:"https://oscardokis.github.io/Recipes-Drinks-with-React/"},
-    {title:"To-Do List", buttonTexts: ["React", "HTML", "JavaScript", "Babel"], img:"/ListTodos.jpg", url:"https://oscardokis.github.io/todo-List-with-React/"},
-    {title:"Tic Tac Toe", buttonTexts: ["React", "CSS", "HTML", "JavaScript"], img:"/TicTacToe.jpg", url:"https://oscardokis.github.io/tic-tac-toe-React-Vite/"},
-    {title:"VideoGame Blind Spot", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"/videoGameJavaScript.jpg", url:"https://oscardokis.github.io/practical-workshop-JavaScript-VideoGames/"},
-    {title:"Password Generator", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"/PasswordGenerator.jpg", url:"https://oscardokis.github.io/Password-Generator/"},
-    {title:"Yard Sale", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"/YardSale.jpg", url:"https://oscardokis.github.io/"},
+    {title:"Recipes and Drinks", buttonTexts: ["React", "Tailwind", "JavaScript", "Vite"], img:"RecipesAndDrinks.jpg", url:"https://oscardokis.github.io/Recipes-Drinks-with-React/"},
+    {title:"To-Do List", buttonTexts: ["React", "HTML", "JavaScript", "Babel"], img:"ListTodos.jpg", url:"https://oscardokis.github.io/todo-List-with-React/"},
+    {title:"Tic Tac Toe", buttonTexts: ["React", "CSS", "HTML", "JavaScript"], img:"TicTacToe.jpg", url:"https://oscardokis.github.io/tic-tac-toe-React-Vite/"},
+    {title:"VideoGame Blind Spot", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"videoGameJavaScript.jpg", url:"https://oscardokis.github.io/practical-workshop-JavaScript-VideoGames/"},
+    {title:"Password Generator", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"PasswordGenerator.jpg", url:"https://oscardokis.github.io/Password-Generator/"},
+    {title:"Yard Sale", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"YardSale.jpg", url:"https://oscardokis.github.io/"},
 ]
