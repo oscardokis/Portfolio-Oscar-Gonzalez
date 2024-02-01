3f4e62d4e095a60512d9bac1eb757acf8dcc8601
Oscar Gonzalez <oscar-gonzalezd1@hotmail.com>
2024-02-01 14:57:01 -0600

    adding old projects to my portfolio

--- src/projects.js
+++ src/projects.js
@@ -3,6 +3,11 @@ export const projects = [
     {title:"To-Do List", buttonTexts: ["React", "HTML", "JavaScript", "Babel"], img:"ListTodos.jpg", url:"https://oscardokis.github.io/todo-List-with-React/"},
     {title:"Tic Tac Toe", buttonTexts: ["React", "CSS", "HTML", "JavaScript"], img:"TicTacToe.jpg", url:"https://oscardokis.github.io/tic-tac-toe-React-Vite/"},
     {title:"VideoGame Blind Spot", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"videoGameJavaScript.jpg", url:"https://oscardokis.github.io/practical-workshop-JavaScript-VideoGames/"},
+    {title:"Netflix Clone", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"NetflixClone.png", url:"https://oscardokis.github.io/API-REST-practico/"},
+    {title:"Mokepon", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"Mokepon.png", url:"https://oscardokis.github.io/Mopekon-JavaScript-Game/"},
     {title:"Password Generator", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"PasswordGenerator.jpg", url:"https://oscardokis.github.io/Password-Generator/"},
     {title:"Yard Sale", buttonTexts: ["HTML", "CSS", "JavaScript"], img:"YardSale.jpg", url:"https://oscardokis.github.io/"},
+    {title:"Google Clone", buttonTexts: ["HTML", "CSS",], img:"GoogleClone.jpg", url:"https://oscardokis.github.io/GoogleClonePractice/"},
+    {title:"Batata", buttonTexts: ["HTML", "CSS"], img:"Batata.jpg", url:"https://oscardokis.github.io/BatataWebsiteVanela/"},
+    {title:"WikipediaClone", buttonTexts: ["HTML", "CSS"], img:"WikipediaClone.png", url:"https://oscardokis.github.io/WikipediaCloneOldRepository/"},
 ]
